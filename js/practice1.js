95af0967650c4c5a2ae6ebceb2a45337bbf3551f
Borys Petriaiev <boris.petryaev@gmail.com>
2022-02-13 09:19:38 +0200

    done

---
+++ js/practice1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''); //const, +prompt
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};// correct
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+    // c = prompt('Один из последних просмотренных фильмов?', ''),
+    // d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// let movies = {
+//     movieName: questionOne,
+//     movieMark: questionTwo
+// };
+console.log(personalMovieDB); // почти правлиьно 
